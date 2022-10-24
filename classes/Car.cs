8b94a968829987c8e404918462ce173f61246ceb
TylerN93 <tylernewton73@gmail.com>
2022-10-24 03:24:32 -0500

    initial commit

---
+++ classes/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace classes
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        public string Make{ get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        }
+    }
+}
+
---
+++ classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyCar = new Car();
+            {
+
+                MyCar.Make = "Ford";
+                MyCar.Model = "F150";
+                MyCar.Year = 2020;
+            }
+
+
+            Console.WriteLine($"{MyCar.Make},{MyCar.Model},{MyCar.Year}");
+            
+
+        }
+    }
+}
+
